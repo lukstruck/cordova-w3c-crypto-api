2808f4a3f3b9a8b65a3a9c71e9086129c6f138ca
lukstruck <lukas.struck@gmx.de>
2019-01-18 23:44:28 +0100

    Implemented first Crypto functionality
    Implemented a bit of HMAC and AES CBC

---
+++ w3c-crypto-api-tests/platforms/ios/HelloCordova/Plugins/w3c-crypto-api/AESEngine.swift
@@ -0,0 +1,8 @@
+//
+//  AESEngine.swift
+//  HelloCordova
+//
+//  Created by Lukas Struck on 18.01.19.
+//
+
+import Foundation
---
+++ w3c-crypto-api-tests/platforms/ios/HelloCordova/Plugins/w3c-crypto-api/HMACEngine.swift
@@ -0,0 +1,8 @@
+//
+//  HMACEngine.swift
+//  HelloCordova
+//
+//  Created by Lukas Struck on 18.01.19.
+//
+
+import Foundation
